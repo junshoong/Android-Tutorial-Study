dfe9507ec9292febc8dca6f8a171fda2c586d84b
Kandinell <kandinell@gmail.com>
2015-03-14 16:56:43 +0900

    Action Bar 적용
    Actionbar 버튼 및 background 및 text 색상 적용

--- Training/app/src/main/java/junsu/training/DisplayMessageActivity.java
+++ Training/app/src/main/java/junsu/training/DisplayMessageActivity.java
@@ -1,22 +1,25 @@
 package junsu.training;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 
-public class DisplayMessageActivity extends ActionBarActivity {
+public class DisplayMessageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        // If your minSdkVersion is 11 or higher, instead use:
+        // getActionBar().setDisplayHomeAsUpEnabled(true);
+
         //Get the message from the intent
         Intent intent = getIntent();
-        String message = intent.getStringExtra(name.EXTRA_MESSAGE);
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Create the text view
         TextView textView = new TextView(this);
--- Training/app/src/main/java/junsu/training/MainActivity.java
+++ Training/app/src/main/java/junsu/training/MainActivity.java
@@ -1,6 +1,6 @@
 package junsu.training;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.EditText;
 
 
-public class name extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     public final static String EXTRA_MESSAGE = "junsu.training.MESSAGE";
 
@@ -17,29 +17,30 @@ public class name extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_name, menu);
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+//                openSearch();
+                return true;
+            case R.id.action_settings:
+//                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     //sendMessag  method add
