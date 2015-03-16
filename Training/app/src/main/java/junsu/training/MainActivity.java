c9687f5bcf2b0521f22d90f5b0867d8ae53a9e54
Kandinell <kandinell@gmail.com>
2015-03-16 18:12:13 +0900

    menu_ name  및 activity_name rename actionbar의 search icon 이 사라지는 문제 해결 (but, 빨간줄이 나옴)

--- Training/app/src/main/java/junsu/training/MainActivity.java
+++ Training/app/src/main/java/junsu/training/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_name);
+        setContentView(R.layout.activity_main);
 
     }
 
